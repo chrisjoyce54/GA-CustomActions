1b4cacfc98fbea8ced9958f1440738585cdc77af
Chris Joyce <CJoyce@jhacorp.com>
2023-01-17 12:52:13 -0600

    Try again

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -1,9 +1,9 @@
 const core = require('@actions/core');
-const gihub = require('@actions/github');
-const exec =  require('@actions/exec');
+const github =  require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from js action!');
+    core.notice('Hello from a custom js action');
 }
 
 run();
